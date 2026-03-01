79cde7fb65878245c1417d12d38e54d89b67932b
RetiredGuitar64 <spider.yuxuan@gmail.com>
2026-03-01 18:10:20 +0800

    init

---
+++ src/main.cr
@@ -0,0 +1,12 @@
+require "termisu"
+
+# t = Termisu.new
+
+#
+# begin
+#   t.set_cell(0, 0, 'H', fg: Termisu::Color.red,   attr: Termisu::Attribute::Bold)
+#   t.render
+# ensure
+#   t.close
+# end
+
